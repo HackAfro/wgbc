4ff727b308d13c090cfc8f503a6d70be711f6225
Richard <richyafro@hotmail.com>
2018-01-02 08:49:20 +0100

    Create home page and init docker

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const browserSync = require('browser-sync');
+const server = browserSync.create();
+const tailwind = require('tailwindcss');
+
+const paths = {
+  scss: {
+    src: 'static/css/build/*.scss',
+    dest: 'static/css/dist/'
+  },
+  tailwind: {
+    src: './config.css',
+    dest: 'static/css/dist/'
+  }
+};
+
+gulp.task('default', ['tailwind', 'build-css', 'watch'], () => {
+  server.init({
+    proxy: 'localhost:8000',
+    files: [paths.scss.dest]
+  });
+});
+
+gulp.task('build', ['tailwind', 'build-css']);
+
+gulp.task(
+  'build-css',
+  () =>
+  gulp
+  .src(paths.scss.src)
+  .pipe(plugins.sass())
+  .pipe(gulp.dest(paths.scss.dest))
+);
+
+gulp.task('reload-css', ['build-css'], () => server.reload());
+gulp.task('reload-tailwind', ['tailwind'], () => server.reload());
+
+gulp.task('tailwind',
+  () =>
+  gulp.src(paths.tailwind.src)
+  .pipe(plugins.postcss([
+    tailwind('./tailwind.js'),
+    require('autoprefixer')
+  ]))
+  .pipe(plugins.rename('styles.css'))
+  .pipe(gulp.dest(paths.tailwind.dest))
+);
+
+
+gulp.task('watch', () => {
+  gulp.watch('static/css/**/*.scss', ['reload-css']);
+  gulp.watch('templates/**/*.html', () => server.reload());
+  gulp.watch(['./config.css', './tailwind.js'], ['reload-tailwind']);
+});
---
+++ static/js/map.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var coordinates = {
+        lat: 4.633864,
+        lng: 7.945659
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: coordinates
+    });
+    var marker = new google.maps.Marker({
+        position: coordinates,
+        map: map
+    });
+}
